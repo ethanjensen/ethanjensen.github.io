8f94a290844d6ff46619fa8dd5f022981a42800e
Ethan Jensen <ethanjensen0919@gmail.com>
2026-03-24 08:36:43 -0500

    testing out mathjax to display latex matrices

--- calculations/calculations.js
+++ calculations/calculations.js
@@ -1,42 +1,5 @@
 import { get_formatted_graded_autos } from './compute_graded_auto_groups.js';
 
-//define data array
-var tabledata = [
-    {id:1, name:"Oli Bob", progress:12, gender:"male", rating:1, col:"red", dob:"19/02/1984", car:1},
-    {id:2, name:"Mary May", progress:1, gender:"female", rating:2, col:"blue", dob:"14/05/1982", car:true},
-    {id:3, name:"Christine Lobowski", progress:42, gender:"female", rating:0, col:"green", dob:"22/05/1982", car:"true"},
-    {id:4, name:"Brendon Philips", progress:100, gender:"male", rating:1, col:"orange", dob:"01/08/1980"},
-    {id:5, name:"Margret Marmajuke", progress:16, gender:"female", rating:5, col:"yellow", dob:"31/01/1999"},
-    {id:6, name:"Frank Harbours", progress:38, gender:"male", rating:4, col:"red", dob:"12/05/1966", car:1},
-];
-
-var table = new Tabulator("#example-table", {
-    data:tabledata,           //load row data from array
-    layout:"fitColumns",      //fit columns to width of table
-    responsiveLayout:"hide",  //hide columns that don't fit on the table
-    addRowPos:"top",          //when adding a new row, add it to the top of the table
-    history:true,             //allow undo and redo actions on the table
-    pagination:"local",       //paginate the data
-    paginationSize:7,         //allow 7 rows per page of data
-    paginationCounter:"rows", //display count of paginated rows in footer
-    movableColumns:true,      //allow column order to be changed
-    initialSort:[             //set the initial sort order of the data
-        {column:"name", dir:"asc"},
-    ],
-    columnDefaults:{
-        tooltip:true,         //show tool tips on cells
-    },
-    columns:[                 //define the table columns
-        {title:"Name", field:"name", editor:"input"},
-        {title:"Task Progress", field:"progress", hozAlign:"left", formatter:"progress", editor:true},
-        {title:"Gender", field:"gender", width:95, editor:"list", editorParams:{values:["male", "female"]}},
-        {title:"Rating", field:"rating", formatter:"star", hozAlign:"center", width:100, editor:true},
-        {title:"Color", field:"col", width:130, editor:"input"},
-        {title:"Date Of Birth", field:"dob", width:130, sorter:"date", hozAlign:"center"},
-        {title:"Driver", field:"car", width:90,  hozAlign:"center", formatter:"tickCross", sorter:"boolean", editor:true},
-    ],
-});
-
 let matrixTable = null;
 
 document.getElementById("generateBtn").addEventListener("click", () => {
@@ -53,7 +16,11 @@ document.getElementById("generateBtn").addEventListener("click", () => {
         columns.push({
             title: `C${j+1}`,
             field: `c${j}`,
-            editor: "input"
+            editor: "input",
+            minWidth: 40,
+            width: 40,
+            maxWidth: 40,
+            hozAlign: "center"
         });
     }
 
@@ -100,6 +67,22 @@ function getMatrix() {
     return matrix;
 }
 
+function matrixToLatex(matrix) {
+    let rows = matrix.map(
+        row => row.join(" & ")
+    ).join(" \\\\ ");
+
+    return `\
+
+\[
+        \\begin{bmatrix}
+        ${rows}
+        \\end{bmatrix}
+    \\]
+
+`;
+}
+
 document.getElementById("processBtn").addEventListener("click", () => {
     const matrix = getMatrix();
 
@@ -110,13 +93,11 @@ document.getElementById("processBtn").addEventListener("click", () => {
 
     const result = get_formatted_graded_autos(matrix);
 
-    document.getElementById("automorphisms").textContent =
-        typeof result === "object"
-            ? JSON.stringify(result, null, 2)
-            : String(result);
+    const latex = matrixToLatex(result);
+    const out = document.getElementById("automorphisms");
+    out.innerHTML = latex;
+    MathJax.typesetPromise();
 });
-
-
 
 
 function download(filename, text) {
